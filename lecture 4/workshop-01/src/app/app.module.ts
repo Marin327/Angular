2b3874df9ef32f5ad66a85b26f3bad3666dafc59
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2023-10-02 20:21:02 +0300

    Add files via upload

---
+++ lecture 4/workshop-01/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentService } from './content.service';
+import { CoreModule } from './core/core.module';
+import { ThemeComponent } from './theme/theme.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+  ],
+  providers: [
+    ContentService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
